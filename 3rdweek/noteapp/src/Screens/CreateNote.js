837a109e6603522b911a60541e13a20edc0ce7e7
nahee-park <jessica396@ewhain.net>
2021-05-13 00:58:34 +0900

    feat: 시간객체 불러외, createNote할 때 만들어질 객체 생성

--- 3rdweek/noteapp/src/Screens/CreateNote.js
+++ 3rdweek/noteapp/src/Screens/CreateNote.js
@@ -1,37 +1,67 @@
 import React, {useState} from "react";
 import Title from '../components/Title';
 import styled from 'styled-components';
-import Button from '../components/Button'
+import Button from '../components/Button';
 
 //로컬스토리지에 입력값 저장하는 곳
 //객체에는 1.제목 2.내용 3.마지막 접근 시간을 저장한다
 function CreateNote () {
-    const [notes,setNotes] = useState({title:'제목',body:'내용',create:'생성시간',update:'업데이트시간'})
+    //로컬스토리지값 가져온 걸로 setNote 할거지만 일단 어떤 요소 들어갈 지 넣어둠 
+    const [note,setNote] = useState({title:'',body:'',create:'',update:'업데이트시간'})
+    const moment = require('moment');
+
+    //타이틀 배열변수에 담음
+    const titleChangeHandler = (event) => {
+        event.preventDefault();
+        console.log("title",event.target.value);
+        setNote({...note,title:event.target.value});
+    }
+
+    //내용 배열변수에 담음
+    const contentChangeHandler = (event) => {
+        event.preventDefault();
+        console.log("content",event.target.value);
+        setNote({...note,body:event.target.value});
+    }
+
+    //submit할 때 그 시간 찍어서 update에 저장, create가 비어있는 경우에만 update로 채우기 
+    const noteSubmit = (event) => {
+        console.log(note);
+        const timestamp = moment().format('YYYY년 MM월DD일 h:m:s');
+        setNote({...note,update:timestamp});
+        if(note.create===''){
+            setNote({...note,create:note.update});
+        }
+    }
+
+
 
     return(
         <CreateNoteWrap>
             <div className="container">
                 <Title/>
                 <Button className="btn-back" color="gray" size="small">Back</Button>
-                <form className="title-form">
+                <div className="title-form">
                     <input
                         className="title-form--input"
                         type="text"
-                        value={notes.title}
-                        placeholder="search"                    
+                        // value={notes.title}
+                        onChange={titleChangeHandler}
+                        placeholder="Title"                    
                     ></input>
-                </form>
-                <form className="content-form">
-                    <input
+                </div>
+                <div className="content-form">
+                    <textarea
                         className="content-form--input"
                         type="text"
-                        value={notes.body}
-                        placeholder="search"                     
-                    ></input>
-                </form>
+                        // value={notes.body}
+                        onChange={contentChangeHandler}
+                        placeholder="Contents"                   
+                    ></textarea>
+                </div>
                 <div className="btn-bottom">
                     <Button className="btn-remove" color="pink" size="large">Remove Note</Button>
-                    <Button className="btn-done" color="gray" size="small">Done</Button>
+                    <Button onClick={noteSubmit} className="btn-done" color="gray" size="small">Done</Button>
                 </div>
             </div>
         </CreateNoteWrap>
@@ -84,8 +114,7 @@ justify-content: center;
         font-style: normal;
         font-weight: normal;
         letter-spacing: 0.03em;
-        margin-left: 10px;
-        margin-right: 10px;
+        padding-left: 15px;
         border: none;
         outline: none;
         width: 591px;
@@ -107,15 +136,16 @@ justify-content: center;
         font-style: normal;
         font-weight: normal;
         letter-spacing: 0.03em;
-        margin-left: 10px;
-        margin-right: 10px;
+        padding-left: 15px;
+        padding-top: 20px;
         border: none;
         outline: none;
-        width: 591px;
-        height: 51px;
+        width: 598px;
+        height: 254px;
         background: #FFFFFF;
         color: #2d2d2d;
         border-radius: 20px;
+        resize: none;
     }
 }
 `;
